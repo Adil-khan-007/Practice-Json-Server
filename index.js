cf80946c89b2a0225dcaa3c95ed7cb2c44b52140
Adil-khan-007 <adilk9030@gmail.com>
2023-01-31 13:17:04 +0530

    Deployment

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 3000;
+// const PORT = 3000;
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+server.listen(PORT, () =>
+  console.log(`JSON Server is running on port ${PORT}`)
+);
